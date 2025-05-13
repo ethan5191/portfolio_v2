c2ceaef2b2c00a8b5f87fff9835bb894d441edee
ethan5191 <ethan5191@hotmail.com>
2025-05-13 07:26:08 -0400

    Moved skills section to its own page

--- app/page.tsx
+++ app/page.tsx
@@ -4,6 +4,7 @@ import AboutSection from "../app/components/aboutMe/AboutSection";
 import MrowsSection from "../app/components/mrows/MrowsSection";
 import McpdtSection from "../app/components/mcpdt/McpdtSection";
 import ProjectsSection from "@/app/components/projects/ProjectsSection";
+import SkillsSection from "@/app/components/skills/SkillsSection";
 import ContactSection from "@/app/components/contact/ContactSection";
 
 export default function Home() {
@@ -21,56 +22,7 @@ export default function Home() {
                     <MrowsSection/>
                     <McpdtSection/>
                     <ProjectsSection/>
-
-                    {/* Skills Section */}
-                    <section id="skills" className={styles.section}>
-                        <h2>Skills</h2>
-                        <div className={styles.projectsGrid}>
-                            <div className={`${styles.skillCategoryCard} ${styles.infoContent}`}>
-                                <h3>Programming Languages</h3>
-                                <ul>
-                                    <li>Java</li>
-                                    <li>JavaScript</li>
-                                    <li>SQL</li>
-                                    <li>MySQL</li>
-                                </ul>
-                            </div>
-                            <div className={`${styles.skillCategoryCard} ${styles.infoContent}`}>
-                                <h3>Frameworks & Libraries</h3>
-                                <ul>
-                                    <li>Spring MVC</li>
-                                    <li>Struts 1.1</li>
-                                    <li>Hibernate/JPA</li>
-                                    <li>Soap Web Services</li>
-                                    <li>jQuery</li>
-                                    <li>React</li>
-                                </ul>
-                            </div>
-                            <div className={`${styles.skillCategoryCard} ${styles.infoContent}`}>
-                                <h3>Tools</h3>
-                                <ul>
-                                    <li>IntelliJ</li>
-                                    <li>Git</li>
-                                    <li>Subversion</li>
-                                    <li>Maven</li>
-                                    <li>Fortify</li>
-                                    <li>GitLab</li>
-                                    <li>JIRA</li>
-                                    <li>JasperSoft Studios</li>
-                                    <li>Soap UI</li>
-                                    <li>WebStorm</li>
-                                </ul>
-                            </div>
-                            <div className={`${styles.skillCategoryCard} ${styles.infoContent}`}>
-                                <h3>Methodologies</h3>
-                                <ul>
-                                    <li>Agile</li>
-                                    <li>Scrum</li>
-                                </ul>
-                            </div>
-                        </div>
-                    </section>
-
+                    <SkillsSection/>
                     <ContactSection/>
                 </div>
                 {/* End .mainContentAndHome */}
