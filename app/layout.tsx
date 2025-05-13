4316f39903e65528fefe84d8c4153703034b2329
ethan5191 <ethan5191@hotmail.com>
2025-05-12 20:56:18 -0400

    Updated text, added new styles, added icons to the header, formatted the header

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,6 @@
 import type {Metadata} from "next";
 import {Geist, Geist_Mono} from "next/font/google";
-import { Analytics } from "@vercel/analytics/next"
+import {Analytics} from "@vercel/analytics/next"
 import "./globals.css";
 
 const geistSans = Geist({
@@ -25,23 +25,36 @@ export default function RootLayout({
 }>) {
     return (
         <html lang="en">
+        <head>
+            <title>Ethan Vanderbur - Portfolio</title>
+            <link
+                rel="stylesheet"
+                href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0/css/all.min.css"
+                integrity="sha512-9usAa10IRO0HhonpyAIVpjrylPvoDwiPUiKdWk5t3PyolY1cOd4DSE0Ga+ri4AuTroPR5aQvXU9xC6qOPnzFeg=="
+                crossOrigin="anonymous"
+                referrerPolicy="no-referrer"
+            />
+        </head>
         <body
             className={`${geistSans.variable} ${geistMono.variable} antialiased`}
         >
         <header>
             <a href="#home" className="header-home-link">
-                Home
+                <i className="fas fa-home"></i>
             </a>
             <nav>
                 <div>
-                    <span>evanderbur3@gmail.com</span> |
-                    <a href="https://www.linkedin.com/in/ethan-vanderbur-2797aa260" target="_blank" rel="noopener noreferrer">LinkedIn</a> |
-                    <span>812-701-6110</span>
+                    <a href="mailto:evanderbur3@gmail.com"><i
+                        className="fas fa-envelope"></i> E.Vanderbur.3@gmail.com</a> |
+                    <a href="https://www.linkedin.com/in/ethan-vanderbur-2797aa260" target="_blank"
+                       rel="noopener noreferrer" className="linkedin-link">
+                        <i className="fab fa-linkedin-in"></i></a> |
+                    <span className="phoneNum"><i className="fas fa-phone"></i> 812-701-6110</span>
                 </div>
             </nav>
         </header>
         {children}
-        <Analytics />
+        <Analytics/>
         </body>
         </html>
     );
