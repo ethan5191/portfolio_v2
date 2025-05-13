061403df5b964eafb4804cabda6696355f9d857d
ethan5191 <ethan5191@hotmail.com>
2025-05-13 05:55:48 -0400

    Moved MROWS and MCPDT sections up and spelled out there name the first time they are shown

--- app/page.tsx
+++ app/page.tsx
@@ -108,6 +108,72 @@ export default function Home() {
                         </div>
                     </section>
 
+                    <section id="mrows" className={styles.section}>
+                        <h2>Marine Resource Order Writing System (MROWS)</h2>
+                        <div className={`${styles.aboutContent} ${styles.infoContent}`}>
+                            <p>MROWS is a monolith Java 8 web application, its main purposes is for the creation and
+                                certification of orders for reserve Marines.</p>
+                            <h4>Related Projects:</h4>
+                            <ul className={styles.accomplishmentsList}>
+                                <li><span className={styles.accomplishmentTitle}>HP Fortify findings:</span>
+                                    <span className={styles.accomplishmentDetails}>Remediated numerous findings to improve
+                                    application security.</span>
+                                </li>
+                                <li><span className={styles.accomplishmentTitle}>Struts 1 to Spring 5:</span>
+                                    <span className={styles.accomplishmentDetails}>Led the migration from Struts 1 to
+                                    Spring 5 to improve performance and application security.</span>
+                                </li>
+                                <li><span className={styles.accomplishmentTitle}>Permissions system refactor:</span>
+                                    <span className={styles.accomplishmentDetails}>Changed the static Java based permissions system into a
+                                    database driven system, allowing for easier and quicker permissions updates.</span>
+                                </li>
+                                <li><span className={styles.accomplishmentTitle}>jQuery inbox migration:</span>
+                                    <span className={styles.accomplishmentDetails}>Led the migration effort to update all inboxes to utilize the
+                                    JavaScript jQuery framework to improve application performance and reduce duplicate
+                                    code.</span>
+                                </li>
+                                <li><span className={styles.accomplishmentTitle}>Report migration:</span>
+                                    <span className={styles.accomplishmentDetails}>Led the migration of our reports from iReport 1.2 to JasperSoft
+                                    Studio improving our report compile times.</span>
+                                </li>
+                            </ul>
+                        </div>
+                    </section>
+
+                    <section id="mcpdt" className={styles.section}>
+                        <h2>Marine Corps Permanent Duty Travel (MCPDT)</h2>
+                        <div className={`${styles.aboutContent} ${styles.infoContent}`}>
+                            <p>MCPDT is a Java 8 web application that owns multiple web services that are called by
+                                other TSO owned systems. It is used to calculate per diem and travel funding lines for PCS and
+                                PCA orders.</p>
+                            <h4>Related Projects:</h4>
+                            <ul className={styles.accomplishmentsList}>
+                                <li><span className={styles.accomplishmentTitle}>End of Year:</span>
+                                    <span className={styles.accomplishmentDetails}>End of fiscal year process involves the creation
+                                    of numerous database scripts to ensure the next FYs funding lines were properly setup.</span>
+                                </li>
+                                <li><span className={styles.accomplishmentTitle}>DAI Reconciliation:</span>
+                                    <span className={styles.accomplishmentDetails}>Created a new web service in MCPDT to take in
+                                    financial data from DTMS and send a reconciliation funding line to DAI. This improves
+                                    MCPDTs financial auditability.</span>
+                                </li>
+                                <li><span className={styles.accomplishmentTitle}>Updates for MROWS PCS travel:</span>
+                                    <span className={styles.accomplishmentDetails}>The 'MROWS - Separation Requests (PCS Travel)'
+                                    project also caused changes in MCPDT. I updated the application to handle the new logic
+                                    flow that existed due to this new process, this was an update to the existing web service.</span>
+                                </li>
+                                <li><span className={styles.accomplishmentTitle}>Report changes:</span>
+                                    <span className={styles.accomplishmentDetails}>Yearly report changes to build new tabs
+                                    on an excel spreadsheet for the new fiscal year to assist with accounting and estimation of funds.</span>
+                                </li>
+                                <li><span className={styles.accomplishmentTitle}>Low cost funding changes:</span>
+                                    <span className={styles.accomplishmentDetails}>Added new funding types to properly account
+                                    for low cost order types. This improved financial auditability and estimation of funds.</span>
+                                </li>
+                            </ul>
+                        </div>
+                    </section>
+
                     {/* Projects Section */}
                     <section id="work" className={styles.section}>
                         <h2>My Work / Projects</h2>
@@ -163,74 +229,6 @@ export default function Home() {
                         </div>
                     </section>
 
-                    <section id="mrows" className={styles.section}>
-                        <h2>MROWS</h2>
-                        <div className={`${styles.aboutContent} ${styles.infoContent}`}>
-                            <p>MROWS is a monolith Java 8 web application, its main purposes is for the creation and
-                                certification of orders for reserve Marines.</p>
-                            <h4>Related Projects:</h4>
-                            <ul className={styles.accomplishmentsList}>
-                                <li><span className={styles.accomplishmentTitle}>HP Fortify findings:</span>
-                                    <span className={styles.accomplishmentDetails}>Remediated numerous findings to improve
-                                    application security.</span>
-                                </li>
-                                <li><span className={styles.accomplishmentTitle}>Struts 1 to Spring 5:</span>
-                                    <span className={styles.accomplishmentDetails}>Led the migration from Struts 1 to
-                                    Spring 5 to improve performance and application security.</span>
-                                </li>
-                                <li><span className={styles.accomplishmentTitle}>Permissions system refactor:</span>
-                                    <span className={styles.accomplishmentDetails}>Changed the static Java based permissions system into a
-                                    database driven system, allowing for easier and quicker permissions updates.</span>
-                                </li>
-                                <li><span className={styles.accomplishmentTitle}>jQuery inbox migration:</span>
-                                    <span className={styles.accomplishmentDetails}>Led the migration effort to update all inboxes to utilize the
-                                    JavaScript jQuery framework to improve application performance and reduce duplicate
-                                    code.</span>
-                                </li>
-                                <li><span className={styles.accomplishmentTitle}>Report migration:</span>
-                                    <span className={styles.accomplishmentDetails}>Led the migration of our reports from iReport 1.2 to JasperSoft
-                                    Studio improving our report compile times.</span>
-                                </li>
-                            </ul>
-                        </div>
-                    </section>
-
-                    <section id="mcpdt" className={styles.section}>
-                        <h2>MCPDT</h2>
-                        <div className={`${styles.aboutContent} ${styles.infoContent}`}>
-                            <p>MCPDT is a Java 8 web application that owns multiple web services that are called by
-                                other
-                                TSO owned systems. It is used to calculate per diem and travel funding lines for PCS and
-                                PCA
-                                orders.</p>
-                            <h4>Related Projects:</h4>
-                            <ul className={styles.accomplishmentsList}>
-                                <li><span className={styles.accomplishmentTitle}>End of Year:</span>
-                                    <span className={styles.accomplishmentDetails}>End of fiscal year process involves the creation
-                                    of numerous database scripts to ensure the next FYs funding lines were properly setup.</span>
-                                </li>
-                                <li><span className={styles.accomplishmentTitle}>DAI Reconciliation:</span>
-                                    <span className={styles.accomplishmentDetails}>Created a new web service in MCPDT to take in
-                                    financial data from DTMS and send a reconciliation funding line to DAI. This improves
-                                    MCPDTs financial auditability.</span>
-                                </li>
-                                <li><span className={styles.accomplishmentTitle}>Updates for MROWS PCS travel:</span>
-                                    <span className={styles.accomplishmentDetails}>The 'MROWS - Separation Requests (PCS Travel)'
-                                    project also caused changes in MCPDT. I updated the application to handle the new logic
-                                    flow that existed due to this new process, this was an update to the existing web service.</span>
-                                </li>
-                                <li><span className={styles.accomplishmentTitle}>Report changes:</span>
-                                    <span className={styles.accomplishmentDetails}>Yearly report changes to build new tabs
-                                    on an excel spreadsheet for the new fiscal year to assist with accounting and estimation of funds.</span>
-                                </li>
-                                <li><span className={styles.accomplishmentTitle}>Low cost funding changes:</span>
-                                    <span className={styles.accomplishmentDetails}>Added new funding types to properly account
-                                    for low cost order types. This improved financial auditability and estimation of funds.</span>
-                                </li>
-                            </ul>
-                        </div>
-                    </section>
-
                     {/* Contact Section */}
                     <section id="contact" className={styles.section}>
                         <h2>Contact</h2>
@@ -256,10 +254,10 @@ export default function Home() {
                 {/* It's a sibling of the main scrolling column */}
                 <aside className={styles.sidebarNav}>
                     <div><a href="#about">ABOUT</a></div>
-                    <div><a href="#work">WORK</a></div>
-                    <div><a href="#skills">SKILLS</a></div>
                     <div><a href="#mrows">MROWS</a></div>
                     <div><a href="#mcpdt">MCPDT</a></div>
+                    <div><a href="#work">WORK</a></div>
+                    <div><a href="#skills">SKILLS</a></div>
                     <div><a href="#contact">CONTACT</a></div>
                 </aside>
 
