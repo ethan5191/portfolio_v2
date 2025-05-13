986249292f4dc33e9880d4ab7e9bd535086e38cc
ethan5191 <ethan5191@hotmail.com>
2025-05-13 07:03:58 -0400

    Moved MCPDT to its own file

--- app/page.tsx
+++ app/page.tsx
@@ -3,6 +3,7 @@ import ProjectCards from './components/projects/ProjectCards';
 import React from "react";
 import AboutSection from "../app/components/aboutMe/AboutSection";
 import MrowsSection from "../app/components/mrows/MrowsSection";
+import McpdtSection from "../app/components/mcpdt/McpdtSection";
 
 // Define a type for your project data structure
 interface Project {
@@ -87,40 +88,7 @@ export default function Home() {
                     {/* About Me Section */}
                     <AboutSection/>
                     <MrowsSection/>
-
-                    <section id="mcpdt" className={styles.section}>
-                        <h2>Marine Corps Permanent Duty Travel (MCPDT)</h2>
-                        <div className={`${styles.aboutContent} ${styles.infoContent}`}>
-                            <p>MCPDT is a Java 8 web application that owns multiple web services that are called by
-                                other TSO owned systems. It is used to calculate per diem and travel funding lines for PCS and
-                                PCA orders.</p>
-                            <h4>Related Projects:</h4>
-                            <ul className={styles.accomplishmentsList}>
-                                <li><span className={styles.accomplishmentTitle}>End of Year:</span>
-                                    <span className={styles.accomplishmentDetails}>End of fiscal year process involves the creation
-                                    of numerous database scripts to ensure the next FYs funding lines were properly setup.</span>
-                                </li>
-                                <li><span className={styles.accomplishmentTitle}>DAI Reconciliation:</span>
-                                    <span className={styles.accomplishmentDetails}>Created a new web service in MCPDT to take in
-                                    financial data from DTMS and send a reconciliation funding line to DAI. This improves
-                                    MCPDTs financial auditability.</span>
-                                </li>
-                                <li><span className={styles.accomplishmentTitle}>Updates for MROWS PCS travel:</span>
-                                    <span className={styles.accomplishmentDetails}>The 'MROWS - Separation Requests (PCS Travel)'
-                                    project also caused changes in MCPDT. I updated the application to handle the new logic
-                                    flow that existed due to this new process, this was an update to the existing web service.</span>
-                                </li>
-                                <li><span className={styles.accomplishmentTitle}>Report changes:</span>
-                                    <span className={styles.accomplishmentDetails}>Yearly report changes to build new tabs
-                                    on an excel spreadsheet for the new fiscal year to assist with accounting and estimation of funds.</span>
-                                </li>
-                                <li><span className={styles.accomplishmentTitle}>Low cost funding changes:</span>
-                                    <span className={styles.accomplishmentDetails}>Added new funding types to properly account
-                                    for low cost order types. This improved financial auditability and estimation of funds.</span>
-                                </li>
-                            </ul>
-                        </div>
-                    </section>
+                    <McpdtSection/>
 
                     {/* Projects Section */}
                     <section id="work" className={styles.section}>
