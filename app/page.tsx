82c86ed698aa34f8c13d48c8569f24c9b608b422
ethan5191 <ethan5191@hotmail.com>
2025-05-12 21:30:35 -0400

    Updated CSS to left aline the projects and skills sections. Also removed a text line and formatted the about me again

--- app/page.tsx
+++ app/page.tsx
@@ -88,37 +88,30 @@ export default function Home() {
                         <div className={`${styles.aboutContent} ${styles.infoContent}`}>
                             <p>I transitioned to software development in my sophomore year of college. Having realized
                                 graphic design wasn't my path, my high school experience with Visual Basic led me to
-                                explore
-                                Java and C/C#. I was immediately drawn to coding, finding my stride particularly with
-                                Java.</p>
+                                explore Java and C/C#. I was immediately drawn to coding, finding my stride particularly
+                                with Java.</p>
                             <p>My subsequent experience as a full-stack Java developer in the United States Marine Corps
-                                has
-                                deeply reinforced what I value most in software development: solving challenging
-                                problems,
-                                building effective systems, and contributing to a meaningful cause. The systems my team
-                                supports directly impact Marines by ensuring accurate and timely pay, a mission I find
-                                particularly rewarding.</p>
+                                has deeply reinforced what I value most in software development: solving challenging
+                                problems, building effective systems, and contributing to a meaningful cause. The
+                                systems my team supports directly impact Marines by ensuring accurate and timely pay, a
+                                mission I find particularly rewarding.</p>
                             <p>My role provided opportunities to apply my growing expertise across various impactful
                                 projects, from developing new modules to facilitating critical system transitions. These
                                 experiences were instrumental in deepening my proficiency with Java, Spring 5, Oracle
-                                SQL,
-                                and Soap web services.</p>
+                                SQL, and Soap web services.</p>
                             <p>In my next position, I am seeking a mid-level engineer role where I can focus extensively
-                                on
-                                daily development with Java, Spring, and SQL. Although I am fully comfortable with the
-                                responsibilities of a senior developer, I am most energized by hands-on problem-solving
-                                within the code itself, a type of work I have had less opportunity to engage in during
-                                recent senior roles.</p>
+                                on daily development with Java, Spring, and SQL. Although I am fully comfortable with
+                                the responsibilities of a senior developer, I am most energized by hands-on
+                                problem-solving within the code itself.</p>
                             <p>You can <a href="/VanderburEthan_2025.pdf" target="_blank"
-                                                   rel="noopener noreferrer">Download my
-                                Resume PDF</a>.</p>
+                                          rel="noopener noreferrer">Download my Resume PDF</a>.</p>
                         </div>
                     </section>
 
                     {/* Projects Section */}
                     <section id="work" className={styles.section}>
                         <h2>My Work / Projects</h2>
-                        <ProjectCards projects={myProjects}/> {/* Pass your project data here */}
+                        <ProjectCards projects={myProjects}/>
                     </section>
 
                     {/* Skills Section */}
@@ -252,7 +245,7 @@ export default function Home() {
                                                     rel="noopener noreferrer">LinkedIn</a>
                                 </p>
                                 <p>Resume: <a href="/VanderburEthan_2025.pdf" target="_blank"
-                                                       rel="noopener noreferrer">
+                                              rel="noopener noreferrer">
                                     Download my Resume PDF</a></p>
                             </div>
                         </div>
