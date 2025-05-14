d9bae24a1910d5085588039627ca6a5cb5edd0fe
ethan5191 <ethan5191@hotmail.com>
2025-05-14 09:27:57 -0400

    Updated about me text, updated section size, updated font-color, changed span to a p tag on home section

--- app/components/home/HomeSection.tsx
+++ app/components/home/HomeSection.tsx
@@ -5,7 +5,7 @@ export default function HomeSection() {
     return (<>
         <section id="home" className={styles.home}>
             <h1>Ethan Vanderbur</h1>
-            <span>Experienced full-stack web developer working with Java, Spring, SQL, and Soap web services.</span>
+            <p>Experienced full-stack web developer working with Java, Spring, SQL, and Soap web services.</p>
         </section>
     </>);
 }
