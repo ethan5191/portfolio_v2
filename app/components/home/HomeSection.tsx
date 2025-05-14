449733e07b0b5634a03c77e3f0df4a72e8ae9d38
ethan5191 <ethan5191@hotmail.com>
2025-05-14 09:52:21 -0400

    Added intro paragraph and styled it. Updated other styles as needed

--- app/components/home/HomeSection.tsx
+++ app/components/home/HomeSection.tsx
@@ -5,7 +5,12 @@ export default function HomeSection() {
     return (<>
         <section id="home" className={styles.home}>
             <h1>Ethan Vanderbur</h1>
-            <p>Experienced full-stack web developer working with Java, Spring, SQL, and Soap web services.</p>
+            <p>Full-Stack Web Developer with 10 Years of Experience.</p>
+            <p className={styles.summaryParagraph}>
+                I'm a passionate full-stack developer with professional experience in the United States Marine
+                Corps. I'm driven by solving complex problems and building scalable systems, leveraging expertise in
+                Java, Spring, and SQL to create software that has real-world impact.
+            </p>
         </section>
     </>);
 }
