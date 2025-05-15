7c9ab4ed1efef29e61f1e9ef37063c42a7cbafb5
ethan5191 <ethan5191@hotmail.com>
2025-05-14 20:59:21 -0400

    Added new Skills section to replace old skills section. WIP still playing with the styles on this one

--- app/page.tsx
+++ app/page.tsx
@@ -5,7 +5,7 @@ import AboutSection from "../app/components/aboutMe/AboutSection";
 import MrowsSection from "../app/components/mrows/MrowsSection";
 import McpdtSection from "../app/components/mcpdt/McpdtSection";
 import Projects2Section from "@/app/components/projects2/Projects2Section";
-import SkillsSection from "@/app/components/skills/SkillsSection";
+import SkillsSection2 from "@/app/components/skills2/SkillsSection2";
 import ContactSection from "@/app/components/contact/ContactSection";
 
 export default function Home() {
@@ -18,7 +18,7 @@ export default function Home() {
                     <MrowsSection/>
                     <McpdtSection/>
                     <Projects2Section/>
-                    <SkillsSection/>
+                    <SkillsSection2/>
                     <ContactSection/>
                 </div>
             </div>
