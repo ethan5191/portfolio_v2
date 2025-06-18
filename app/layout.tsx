12a0ad7a3312b7d295e60cd2009fd14e638acab2
ethan5191 <ethan5191@hotmail.com>
2025-06-18 16:30:41 -0400

    Updated font and removed old font variables

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,6 @@
-// app/layout.tsx
 'use client';
 
-import {Geist, Geist_Mono} from "next/font/google";
+import { Inter } from "next/font/google";
 import {Analytics} from "@vercel/analytics/next"
 import "./globals.css";
 import React from 'react';
@@ -9,19 +8,13 @@ import ScrollIndicator from "@/app/components/scroll/ScrollIndicator";
 import './variables.css';
 import Header from "@/app/components/header/Header";
 import Footer from "@/app/components/footer/Footer";
-
 import '@fortawesome/fontawesome-svg-core/styles.css';
 import {config} from '@fortawesome/fontawesome-svg-core';
 
 config.autoAddCss = false;
 
-const geistSans = Geist({
-    variable: "--font-geist-sans",
-    subsets: ["latin"],
-});
-
-const geistMono = Geist_Mono({
-    variable: "--font-geist-mono",
+const inter = Inter({
+    variable: "--font-inter",
     subsets: ["latin"],
 });
 
@@ -32,19 +25,12 @@ export default function RootLayout({
 }>) {
 
     return (
-        <html lang="en">
+        <html lang="en" className={inter.variable}>
         <head>
             <title>Ethan Vanderbur - Portfolio</title>
             <meta name="description" content="Full-Stack Java web developer portfolio"/>
-            <link
-                rel="stylesheet"
-                href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0/css/all.min.css"
-                integrity="sha512-9usAa10IRO0HhonpyAIVpjrylPvoDwiPUiKdWk5t3PyolY1cOd4DSE0Ga+ri4AuTroPR5aQvXU9xC6qOPnzFeg=="
-                crossOrigin="anonymous"
-                referrerPolicy="no-referrer"
-            />
         </head>
-        <body className={`${geistSans.variable} ${geistMono.variable} antialiased dark-mode`}>
+        <body className={`antialiased dark-mode`}>
         <Header/>
         <main>
             {children}
