80acdfadbe0a45321267330bd3264ba8e155ba9d
ethan5191 <ethan5191@hotmail.com>
2025-06-19 14:26:21 -0400

    Updated MROWS section to use new styles. Added tooltip for acronym. Updated order of MROWS elements

--- app/components/mrows/MrowsSection.tsx
+++ app/components/mrows/MrowsSection.tsx
@@ -1,22 +1,26 @@
 import accomplishments from "../shared/accomplishmentList.module.css";
-import sectionStyles from "../shared/sectionContent.module.css";
 import React from "react";
+import sectionBase from "@/app/components/shared/sectionBase.module.css";
+import styles from './mrowsSection.module.css';
 
 export default function MrowsSection() {
-    return (<section id="mrows" className={sectionStyles.section}>
-        <h2>Marine Resource Order Writing System (MROWS)</h2>
-        <div className={`${sectionStyles.sectionContentWrapper} ${sectionStyles.infoContent}`}>
-            <p>MROWS is a monolithic Java 8 web application used to create and certify orders for reserve Marines.</p>
+    return (<section id="mrows" className={`${sectionBase.sectionBase} ${styles.mrowsSection}`}>
+        <h2>MROWS
+            <span className={styles.acronymTooltip}>Marine Resource Order Writing System</span>
+        </h2>
+        <div className={styles.contentColumn}>
+            <p className={styles.acronymDefinition}>MROWS is a monolithic Java 8 web application used to create and
+                certify orders for reserve Marines.</p>
             <h4>Related Projects:</h4>
             <ul className={accomplishments.accomplishmentsList}>
-                <li><span className={accomplishments.accomplishmentTitle}>Security Remediation:</span>
-                    <span className={accomplishments.accomplishmentDetails}>Resolved multiple HP Fortify findings to strengthen application security.</span>
-                </li>
                 <li><span className={accomplishments.accomplishmentTitle}>Framework Migration:</span>
                     <span className={accomplishments.accomplishmentDetails}>Led migration from Struts 1 to Spring 5, improving performance and maintainability.</span>
                 </li>
                 <li><span className={accomplishments.accomplishmentTitle}>Permissions Refactor:</span>
                     <span className={accomplishments.accomplishmentDetails}>Rebuilt static permission logic into a dynamic, database-driven system for easier management.</span>
+                </li>
+                <li><span className={accomplishments.accomplishmentTitle}>Security Remediation:</span>
+                    <span className={accomplishments.accomplishmentDetails}>Resolved multiple HP Fortify findings to strengthen application security.</span>
                 </li>
                 <li><span className={accomplishments.accomplishmentTitle}>Inbox Optimization:</span>
                     <span className={accomplishments.accomplishmentDetails}>Upgraded all inbox components to jQuery for better performance and reduced code duplication..</span>
