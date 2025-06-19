ec5eeefdffa2ab535b2707f23ac4cfe60c4ad1c1
ethan5191 <ethan5191@hotmail.com>
2025-06-19 14:58:34 -0400

    Updated contact section to use new styles. ADded new font awesome library. Removed the display flex from the baseSection as it was doing stuff that was not intended

--- app/components/contact/ContactSection.tsx
+++ app/components/contact/ContactSection.tsx
@@ -1,24 +1,29 @@
 import contact from "./contactSection.module.css";
-import sectionStyles from "../shared/sectionContent.module.css";
 import React from "react";
+import sectionBase from "@/app/components/shared/sectionBase.module.css";
+import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
+import {faEnvelope, faFilePdf, faPhone} from "@fortawesome/free-solid-svg-icons";
+import {faLinkedin} from '@fortawesome/free-brands-svg-icons';
+import styles from "@/app/components/home/HomeSection.module.css";
 
 export default function ContactSection() {
-    return (<section id="contact" className={sectionStyles.section}>
-            <h2>Contact</h2>
-            <div className={`${contact.contactContent} ${sectionStyles.infoContent}`}>
-                <h3>Ethan Vanderbur</h3>
-                <p>Columbus, Indiana</p>
-                <div className={contact.contactMethods}>
-                    <p><i className="fas fa-phone"></i> (812)-701-6110</p>
-                    <p><i className="fas fa-envelope"></i> <a href="mailto:evanderbur3@gmail.com">E.Vanderbur.3@gmail.com</a></p>
-                    <p><i className="fab fa-linkedin"></i> <a href="https://www.linkedin.com/in/ethan-vanderbur-2797aa260/"
-                                        target="_blank"
-                                        rel="noopener noreferrer">LinkedIn</a>
-                    </p>
-                    <p><i className="fas fa-file-pdf"></i> <a href="/VanderburEthan_2025_V3.pdf" target="_blank"
-                                                              rel="noopener noreferrer">
-                        Download my Resume PDF</a></p>
-                </div>
+    return (<section id="contact" className={`${sectionBase.sectionBase} ${contact.contentSection}`}>
+        <h2>Contact</h2>
+        <div>
+            <h1 className={styles.myName}>Ethan Vanderbur</h1>
+            <p>Columbus, Indiana</p>
+            <div className={contact.contactMethods}>
+                <p><FontAwesomeIcon icon={faPhone}/> (812)-701-6110</p>
+                <p><FontAwesomeIcon icon={faEnvelope}/> <a
+                    href="mailto:evanderbur3@gmail.com">E.Vanderbur.3@gmail.com</a></p>
+                <p><FontAwesomeIcon icon={faLinkedin}/> <a href="https://www.linkedin.com/in/ethan-vanderbur-2797aa260/"
+                                                           target="_blank"
+                                                           rel="noopener noreferrer">LinkedIn</a>
+                </p>
+                <p><FontAwesomeIcon icon={faFilePdf}/> <a href="/VanderburEthan_2025_V3.pdf" target="_blank"
+                                                          rel="noopener noreferrer">
+                    Download my Resume PDF</a></p>
             </div>
-        </section>);
+        </div>
+    </section>);
 }
